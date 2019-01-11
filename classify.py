578629af020727dcbda8c0c353dcea8c152d7960
Davon Prewitt <prewittdavon@gmail.com>
2019-01-11 14:09:41 -0500

    Removes comments and auth tokens.

--- classify.py
+++ classify.py
@@ -19,8 +19,8 @@ with tf.gfile.GFile("tf_files/retrained_graph.pb", 'rb') as f:
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
 
-account_sid = "AC0b6567ca0ee1a1509f43d9a2031b2b16"
-auth_token = "051afdccb7d835ad6d2acfec57341b96"
+account_sid = "[ACCOUNT SSID]"
+auth_token = "[AUTH TOKEN]"
 client = Client(account_sid, auth_token)
 
 # Find these values at https://twilio.com/user/account
@@ -82,38 +82,6 @@ with tf.Session() as sess:
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-camera.release()
-cv2.destroyAllWindows()
-#if image_path:
-#
-#    # Read the image_data
-#    image_data = tf.gfile.FastGFile(image_path, 'rb').read()
-#    print(type(image_data))
-#
-#    # Loads label file, strips off carriage return
-#    label_lines = [line.rstrip() for line
-#                       in tf.gfile.GFile("tf_files/retrained_labels.txt")]
-#
-#    # Unpersists graph from file
-#    with tf.gfile.FastGFile("tf_files/retrained_graph.pb", 'rb') as f:
-#        graph_def = tf.GraphDef()
-#        graph_def.ParseFromString(f.read())
-#        _ = tf.import_graph_def(graph_def, name='')
-#
-#    with tf.Session() as sess:
-#        # Feed the image_data as input to the graph and get first prediction
-#        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-#
-#        predictions = sess.run(softmax_tensor, \
-#                 {'DecodeJpeg/contents:0': image_data})
-#
-#        # Sort to show labels of first prediction in order of confidence
-#        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
-#        for node_id in top_k:
-#            human_string = label_lines[node_id]
-#            score = predictions[0][node_id]
-#            print('%s (score = %.5f)' % (human_string, score))
 
 camera.release()
 cv2.destroyAllWindows()
